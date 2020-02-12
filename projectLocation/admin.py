d46691af7be111ce877ed305d5434c622775da1b
yeco <oscco.yoshin@gmail.com>
2020-02-11 19:57:46 -0500

    adding additional characteristics for models

--- projectLocation/admin.py
+++ projectLocation/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Localidad, Proyecto
 
 # Register your models here.
+admin.site.register(Localidad, admin.ModelAdmin)
+admin.site.register(Proyecto, admin.ModelAdmin)
--- projectLocation/models.py
+++ projectLocation/models.py
@@ -12,7 +12,9 @@ class Localidad (models.Model):
 class Proyecto (gismodels.Model):
 	nombre = models.CharField(max_length=100)
 	ubicacion = gismodels.PointField()
-	
+	localidad = models.ForeignKey(Localidad,on_delete=models.CASCADE)
+	fecha_creacion = models.DateField(blank=True)
+
 
     def __str__(self):
         return self.nombre
