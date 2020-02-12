a8ab7e1f0c6d838fca80a76727d0881b9d078353
yeco <oscco.yoshin@gmail.com>
2020-02-11 20:07:03 -0500

    run migrations

--- projectLocation/models.py
+++ projectLocation/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.contrib.gis import models as gismodels
+from django.contrib.gis.db import models as gismodels
 # Create your models here.
 
 
 class Localidad (models.Model):
 	nombre = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.nombre
+	def __str__(self):
+		return self.nombre
+
+	class Meta:
+		verbose_name_plural = "Localidades"
 
 class Proyecto (gismodels.Model):
 	nombre = models.CharField(max_length=100)
@@ -15,6 +18,5 @@ class Proyecto (gismodels.Model):
 	localidad = models.ForeignKey(Localidad,on_delete=models.CASCADE)
 	fecha_creacion = models.DateField(blank=True)
 
-
-    def __str__(self):
-        return self.nombre
+	def __str__(self):
+		return self.nombre
