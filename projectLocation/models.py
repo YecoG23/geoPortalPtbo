ce27537394d65c8c9948c08a1484f315019f2c72
yeco <oscco.yoshin@gmail.com>
2020-02-11 19:51:03 -0500

    creating localidad and proyecto models

--- projectLocation/models.py
+++ projectLocation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.gis import models as gismodels
 # Create your models here.
+
+
+class Localidad (models.Model):
+	nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Proyecto (gismodels.Model):
+	nombre = models.CharField(max_length=100)
+	ubicacion = gismodels.PointField()
+	
+
+    def __str__(self):
+        return self.nombre
