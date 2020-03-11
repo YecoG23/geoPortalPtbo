6edfa875ef9319f6719163cced1dbd8d68c7ed1d
Yeco Master <oscco.yoshin@gmail.com>
2020-03-10 22:06:53 -0500

    changed wsgi file to settings productions

--- geoPortalPtbo/wsgi.py
+++ geoPortalPtbo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoPortalPtbo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoPortalPtbo.settings.production')
 
 application = get_wsgi_application()
