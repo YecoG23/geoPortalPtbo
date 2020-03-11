be8452c4cda4db15da081207901a3d0ce7dd9ea6
Yeco Master <oscco.yoshin@gmail.com>
2020-03-10 23:10:25 -0500

    gdal for production heroku config

--- geoPortalPtbo/settings/common.py
+++ geoPortalPtbo/settings/common.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+vzp#4mpnbzcewa%v3u(l788u@+0974+7t5^_=k$u(c^=o83)#'
+SECRET_KEY = os.environ.get('SECRET_KEY','+vzp#4mpnbzcewa%v3u(l788u@+0974+7t5^_=k$u(c^=o83)#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
